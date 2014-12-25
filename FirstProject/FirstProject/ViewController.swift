01962d0e317c6a05be9cbb9f4f4d338ecc6aa457
peleg.aran <peleg.aran@gmail.com>
2014-12-25 13:26:48 +0200

    fdfd

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("fdfd")
+        println("peleg aran aran")
     }
 
     override func didReceiveMemoryWarning() {
