420af46f32b07f627ac0a6ce185549f695c773a9
peleg.aran <peleg.aran@gmail.com>
2014-12-25 13:13:37 +0200

    dsds

---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Peleg Aran on 12/25/14.
+//  Copyright (c) 2014 Peleg Aran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("fdfd")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
